a10702b8941fa4c3c8293fff31628ed4ac33895a
Luis Noriega <luisnoriegag94@gmail.com>
2017-08-01 12:34:42 -0500

    se modifico 'bjnd_zhao()'

--- test_Zhao.py
+++ test_Zhao.py
@@ -22,20 +22,20 @@ def bjnd_image(path, filename):
     image = background_avg(gray_image)
 
     # Calculate BJND values
-    bjnd_img_zhao= bjnd(image, edges)
+    bjnd_img_zhao= bjnd_zhao(image, edges)
     print filename, numbits(bjnd_img_zhao)
     
 if __name__=='__main__':   
-    '''filenames_left = ['Adirondack0.png','ArtL0.png','Jadeplant0.png','Motorcycle0.png','MotorcycleE0.png','Piano0.png','PianoL0.png',
-                 'Pipes0.png','Playroom0.png','Playtable0.png','PlaytableP0.png','Recycle0.png','Shelves0.png','Teddy0.png','Vintage0.png']'''
+    filenames_left = ['Adirondack0.png','ArtL0.png','Jadeplant0.png','Motorcycle0.png','MotorcycleE0.png','Piano0.png','PianoL0.png',
+                 'Pipes0.png','Playroom0.png','Playtable0.png','PlaytableP0.png','Recycle0.png','Shelves0.png','Teddy0.png','Vintage0.png']
     
-    filenames_right = ['Adirondack1.png','ArtL1.png','Jadeplant1.png','Motorcycle1.png','MotorcycleE1.png','Piano1.png','PianoL1.png',
-                 'Pipes1.png','Playroom1.png','Playtable1.png','PlaytableP1.png','Recycle1.png','Shelves1.png','Teddy1.png','Vintage1.png']
+    ''' filenames_right = ['Adirondack1.png','ArtL1.png','Jadeplant1.png','Motorcycle1.png','MotorcycleE1.png','Piano1.png','PianoL1.png',
+                 'Pipes1.png','Playroom1.png','Playtable1.png','PlaytableP1.png','Recycle1.png','Shelves1.png','Teddy1.png','Vintage1.png'] '''
     
     path = '.\Yimages\\'
     '''for name in filenames_left:
         get_image_modified(path, name)'''
         
-    for name in filenames_right:
+    for name in filenames_left:
         bjnd_image(path, name)
     
